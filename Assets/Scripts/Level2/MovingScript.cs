30f70b4b7838587bcbec1f7b746ded7a955b381a
Rania Wael <rania.waelsameh@yahoo.com>
2017-12-04 18:43:41 +0200

    Level2 basic environment and an obstacle

--- Assets/Scripts/Level2/MovingScript.cs
+++ Assets/Scripts/Level2/MovingScript.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MovingScript : MonoBehaviour {
+	//NavMeshAgent nav;
+	public GameObject floor;
 
 	// Use this for initialization
 	void Start () {
-		
+		//nav = GetComponent <NavMeshAgent> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Input.GetKey (KeyCode.UpArrow);
+		float z = Input.GetAxis("Vertical");
+		float x = Input.GetAxis("Horizontal");
+		gameObject.transform.Translate (0, 0, z);
+		gameObject.transform.Rotate (0, x, 0);
+		//nav.SetDestination (new Vector3(x, -floor.transform.position.y, z));
 	}
 }
